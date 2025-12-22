3efeaf3c26a8bc9c7e4b84186bbcd5bf731b2a3b
padapook <pluk.aphiwat@gmail.com>
2025-12-22 11:50:44 +0700

    feat: init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletJa {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletJa.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletJa {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
