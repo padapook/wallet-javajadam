350dc13741b3c39e4cab5d7b1bc652302da8c8cd
padapook <pluk.aphiwat@gmail.com>
2025-12-28 15:31:19 +0700

    feat(auth): เพิ่มการ encode password โดยใช้ Argon2

--- src/main/java/com/example/walletja/repository/UserRepository.java
+++ src/main/java/com/example/walletja/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.example.walletja.repository;
 
 import com.example.walletja.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query; 
 import org.springframework.stereotype.Repository;
+import java.util.List;                             
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT * FROM User WHERE isDeleted = false")
+
+    @Query("SELECT u FROM User u WHERE u.isDeleted = false")
     List<User> findAllActiveUsers();
 }
---
+++ src/main/java/com/example/walletja/util/PasswordUtil.java
@@ -0,0 +1,17 @@
+package com.example.walletja.util;
+
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordUtil {
+        private final Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(16, 32, 1, (1024*64), 3);
+
+        public String hashPassword(String rawPassword) {
+            return encoder.encode(rawPassword);
+        }
+
+        public Boolean verifyPassword(String rawPassword, String encodedPassword) {
+            return encoder.matches(rawPassword, encodedPassword);
+        }
+}
