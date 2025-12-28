41ff95806de643a4c5750989a75e418db57f99ef
padapook <pluk.aphiwat@gmail.com>
2025-12-28 19:56:34 +0700

    .

--- src/main/java/com/example/walletja/WalletJa.java
+++ src/main/java/com/example/walletja/WalletJa.java
@@ -3,6 +3,7 @@ package com.example.walletja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableJpaAuditing
